af0aa5f7c8a03a4f60f6914fa2183b72fb5d5b2b
adoo <Adoo@outlook.com>
2020-04-29 00:37:13 +0800

    feat: merge layer

--- canvas/src/ctx_2d.rs
+++ canvas/src/ctx_2d.rs
@@ -1,8 +1,35 @@
 use crate::*;
-pub struct CanvasRenderingContext2D {
-  layer: Rendering2DLayer,
+pub struct CanvasRenderingContext2D<'a> {
+  canvas: &'a mut Canvas,
+  buffer: LayerBuffer,
 }
 
-impl CanvasRenderingContext2D {
-  pub fn compos_layer(&mut self, layer: Rendering2DLayer) { unimplemented!() }
+pub struct DrawInfo {}
+
+impl<'a> CanvasRenderingContext2D<'a> {
+  /// Create a new layer to drawing, and not effect current layer.
+  #[inline]
+  pub fn new_layer(&self) -> Rendering2DLayer { Rendering2DLayer::new() }
+
+  /// Compose a layer into current drawing.
+  pub fn compose_layer(&mut self, other_layer: Rendering2DLayer) -> &mut Self {
+    self.compose_layer_buffer(&other_layer.finish())
+  }
+
+  /// Compose a layer buffer into current drawing. Layer buffer is the result
+  /// after a layer drawing finished.
+  #[inline]
+  pub fn compose_layer_buffer(&mut self, buffer: &LayerBuffer) -> &mut Self {
+    unimplemented!();
+    if self.buffer.mergeable(buffer) {
+      self.buffer.merge(&buffer)
+    }
+
+    self
+  }
+
+  /// Commit all composed layer to gpu for painting on screen.
+  pub fn commit(&mut self) -> DrawInfo {
+    unimplemented!();
+  }
 }
--- canvas/src/lib.rs
+++ canvas/src/lib.rs
@@ -1,4 +1,6 @@
+mod canvas;
 mod ctx_2d;
 mod layer;
+pub use canvas::*;
 pub use ctx_2d::*;
 pub use layer::*;
