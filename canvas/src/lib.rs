6c2d12225b37928b7d97f03af8abc3efadd530e2
adoo <Adoo@outlook.com>
2020-05-06 22:15:19 +0800

    feat(canvas): canvas 2d use a coordinate system like web 2d

--- canvas/src/lib.rs
+++ canvas/src/lib.rs
@@ -1,5 +1,20 @@
 #![feature(decl_macro)]
 mod canvas;
-mod layer;
+mod layer_2d;
 pub use crate::canvas::*;
-pub use layer::*;
+pub use layer_2d::*;
+
+/// The tag for device unit system to prevent mixing values from different
+/// system.
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct DeviceUnit;
+
+/// The tag for logic unit system to prevent mixing values from different
+/// system.
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct LogicUnit;
+
+pub type Rect = euclid::Rect<f32, LogicUnit>;
+pub type Point = euclid::Point2D<f32, LogicUnit>;
+pub type Size = euclid::Size2D<f32, LogicUnit>;
+pub type Transform = euclid::Transform2D<f32, LogicUnit, LogicUnit>;
--- canvas/src/shaders/geometry.vert
+++ canvas/src/shaders/geometry.vert
@@ -2,10 +2,28 @@
 
 layout(location=0) in vec2 pos;
 layout(location=1) in uint prim_id;
-
 layout(location=0) out vec3 v_color;
 
+layout(set=0, binding=0) 
+uniform CoordinateConvertMatrix {
+     float m11;
+     float m12;
+     float m21;
+     float m22;
+     float m31;
+     float m32;
+};
+
+
 void main() {
+    mat3x2 axis_map = mat3x2(
+        m11, m12,
+        m21, m22,
+        m31, m32
+    );
+
     v_color = vec3(0.5, 0.0, 0.5);
-    gl_Position = vec4(pos, 0, 1.0);
+    vec2 mapped = axis_map * vec3( pos, 1);
+    gl_Position = vec4(mapped, 0, 1.0);
+
 }
