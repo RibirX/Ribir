a6bbf16d671f36d162a76b244d4785e31390db4f
wjian23 <w_jian23@163.com>
2020-06-03 23:33:34 +0800

    fix lint error, exceeded maximum width
    Change-Id: If8fb933e4463539e5d4aa28b950ae7210f6bc4aa

--- core/src/util/proxy_macro.rs
+++ core/src/util/proxy_macro.rs
@@ -1,11 +1,22 @@
-pub macro proxy_immut_impl($method: ident, {$($path:ident).*}, $type: ty, $call_method: ident, ($($arg:ident : $arg_type: ty),*)) {
+pub macro proxy_immut_impl(
+    $method: ident, {$($path:ident).*},
+    $type: ty,
+    $call_method: ident,
+    ($($arg:ident : $arg_type: ty),*))
+{
     #[inline]
     fn $method(&self $(, $arg: $arg_type)*)-> $type {
         self.$($path).* .$call_method($($arg,)*)
     }
 }
 
-pub macro proxy_mut_impl($method: ident, {$($path:ident).*}, $type: ty, $call_method: ident, ($($arg:ident : $arg_type: ty),*)) {
+pub macro proxy_mut_impl(
+    $method: ident,
+    {$($path:ident).*},
+    $type: ty,
+    $call_method: ident,
+    ($($arg:ident : $arg_type: ty),*))
+{
     #[inline]
     fn $method(&mut self $(, $arg: $arg_type)*) -> $type {
         self. $($path).* .$call_method($($arg,)*)
