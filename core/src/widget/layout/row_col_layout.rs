c587fb251f2f06f6ccc8adcaac31dd1ec5eca05b
adoo <Adoo@outlook.com>
2020-06-26 10:22:33 +0800

    refactor: rennderObject use associated type
    After widget alwyas have a 'static lifetime, RnderObject can use associated type and not depends on
    GAT

--- core/src/widget/layout/row_col_layout.rs
+++ core/src/widget/layout/row_col_layout.rs
@@ -51,7 +51,8 @@ impl MultiChildWidget for RowColumn {
   fn take_children(&mut self) -> Vec<Box<dyn Widget>> { std::mem::take(&mut self.children) }
 }
 
-impl RenderObject<RowColumn> for RowColRender {
+impl RenderObject for RowColRender {
+  type Owner = RowColumn;
   fn update(&mut self, _owner_widget: &RowColumn) {}
 
   fn perform_layout(&mut self, id: RenderId, ctx: &mut RenderCtx) -> Size {
