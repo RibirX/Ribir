b895dd1e53ada356bb4fc89ba88ce944d757d85b
adoo <Adoo@outlook.com>
2020-08-23 00:30:54 +0800

    fix: use stable rxrust & wgpu

--- canvas/src/wgpu_render.rs
+++ canvas/src/wgpu_render.rs
@@ -392,6 +392,7 @@ fn create_render_pipeline(
   ));
 
   device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+    label: Some("render pipeline"),
     layout: Some(&render_pipeline_layout),
     vertex_stage: wgpu::ProgrammableStageDescriptor {
       module: &vs_module,
