897432d9604c5523d192978914076292d0ab9f60
Adoo <Adoo@outlook.com>
2022-04-17 18:38:45 +0800

    refactor(gpu): 💡 remove unused shaders

--- gpu/src/wgpu_gl/builder_shaders.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-glslangValidator -V ./shaders/geometry.vert -o ./shaders/geometry.vert.spv
-glslangValidator -V ./shaders/geometry.frag -o ./shaders/geometry.frag.spv
-glslangValidator -V ./shaders/bgra_2_rgba.comp -o ./shaders/bgra_2_rgba.comp.spv
-
