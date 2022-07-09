7ac6bd235a36ffce457a7df8a87414f174ce0f88
Adoo <Adoo@outlook.com>
2022-07-09 17:36:25 +0800

    fix(core): 🐛 icon should fit child size and center align it

--- core/src/widget/icon.rs
+++ core/src/widget/icon.rs
@@ -7,16 +7,16 @@ pub struct Icon {
 
 impl ComposeSingleChild for Icon {
   fn compose_single_child(this: Stateful<Self>, child: Option<Widget>, _: &mut BuildCtx) -> Widget {
-    // todo:
-    // Maybe we can not subscribe the stateful `this` in codegen if nobody want to
-    // modify `this`, across compare its ref count and the count of who just
-    // follow its change .  So user can directly track but codegen will not
-    // subscribe if no triggers have.
     widget! {
       track { this }
       SizedBox {
         size: this.size,
-        ExprWidget { expr: child }
+        ExprWidget {
+          expr: child,
+          box_fit: BoxFit::Contain,
+          h_align: HAlign::Center,
+          v_align: VAlign::Center,
+        }
       }
     }
   }
