1f7c32673fde40014cc239ec3beed414ee656334
Adoo <Adoo@outlook.com>
2022-08-26 23:44:55 +0800

    refactor(core): 💡 split `Anchor` into `LeftAnchor`,`RightAcnhor`,`TopAnchor` and `BottomAnchor`

--- core/src/test.rs
+++ core/src/test.rs
@@ -24,6 +24,56 @@ pub fn root_and_children_rect(wnd: &Window) -> (Rect, Vec<Rect>) {
   (rect, children_box_rect)
 }
 
+#[derive(Default)]
+pub struct ExpectRect {
+  pub x: Option<f32>,
+  pub y: Option<f32>,
+  pub width: Option<f32>,
+  pub height: Option<f32>,
+}
+pub struct LayoutTestItem<'a> {
+  pub path: &'a [usize],
+  pub expect: ExpectRect,
+}
+
+pub fn expect_layout_result(wnd_size: Size, w: Widget, items: &[LayoutTestItem]) {
+  let mut wnd = Window::without_render(w, wnd_size);
+  wnd.draw_frame();
+  items.iter().for_each(|LayoutTestItem { path, expect }| {
+    let res = layout_info_by_path(&wnd, path);
+    if let Some(x) = expect.x {
+      assert_eq!(x, res.min_x());
+    }
+    if let Some(y) = expect.y {
+      assert_eq!(y, res.min_y());
+    }
+    if let Some(width) = expect.width {
+      assert_eq!(width, res.width())
+    }
+
+    if let Some(height) = expect.height {
+      assert_eq!(height, res.height())
+    }
+  });
+}
+
+/// ues a index path to access widget tree and return the layout info,
+/// [0, 1] means use the second child of the root.
+/// [0, 1, 2] the first node at the root level (must be 0), then down to its
+/// second child, then down to third child.
+pub fn layout_info_by_path(wnd: &Window, path: &[usize]) -> Rect {
+  assert_eq!(path[0], 0);
+  let tree = &wnd.widget_tree;
+  let mut node = tree.root();
+  for (level, idx) in path[1..].into_iter().enumerate() {
+    node = node.children(tree).skip(*idx).next().unwrap_or_else(|| {
+      panic!("node no exist: {:?}", &path[0..level]);
+    });
+  }
+
+  tree.layout_box_rect(node).unwrap()
+}
+
 impl Window {
   #[inline]
   pub fn widget_count(&self) -> usize { self.widget_tree.count() }
