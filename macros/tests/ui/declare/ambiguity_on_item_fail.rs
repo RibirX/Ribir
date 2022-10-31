a08677e5aa6abe408e4efd24629080afbe1f3e15
Adoo <Adoo@outlook.com>
2022-10-31 20:48:53 +0800

    ce(macros): 🔧 improve compile error of ambiguity observe expression of on item

---
+++ macros/tests/ui/declare/ambiguity_on_item_fail.rs
@@ -0,0 +1,20 @@
+use ribir::prelude::*;
+
+fn main() {
+  let miss_block = widget! {
+    Container {
+      id: container,
+      size: Size::zero() }
+    on container.size
+  };
+
+  let ambiguity = widget! {
+    track { flag: true.into_stateful() }
+    Container {
+      size: Size::zero()
+    }
+    on *flag {
+      change: |_, _| {}
+    }
+  };
+}
