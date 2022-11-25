e30b97fb303a71306a4bd2fc2bea6dc84cfc2e9d
sologeek <liyoushan3@gmail.com>
2022-11-25 15:41:55 +0800

    refactor(widgets): 💡 tabs refactor

--- widgets/src/tabs.rs
+++ widgets/src/tabs.rs
@@ -8,12 +8,17 @@ pub struct Tabs {
 }
 
 #[derive(Declare, Debug)]
-pub struct InkBarStyle;
+pub struct InkBarStyle {
+  pub ink_bar_rect: Rect,
+}
 
 impl ComposeStyle for InkBarStyle {
-  type Host = Widget;
+  type Host = Option<Widget>;
   #[inline]
-  fn compose_style(_: Stateful<Self>, host: Widget) -> Widget { host }
+  fn compose_style(_: Stateful<Self>, host: Option<Widget>) -> Widget { 
+    assert!(host.is_none());
+    Void.into_widget()  
+  }
 }
 
 #[derive(Template)]
@@ -41,55 +46,55 @@ impl ComposeChild for Tabs {
       panes.push(pane.child);
     }
 
-    let tab_size = panes.len();
-
     widget! {
       track {
-        this: this.into_stateful()
+        this: this.into_stateful(),
+        active_header_rect: Rect::zero().into_stateful()
       }
 
       Column {
-        LayoutBox {
-          id: stack,
-          Stack {
-            Row {
-              border: Border::only_bottom(BorderSide {
-                width: 1., color: Palette::of(ctx).primary()
-              }),
-              DynWidget {
-                dyns: {
-                  headers.into_iter()
-                    .enumerate()
-                    .map(move |(idx, header)| {
-                      widget! {
-                        Expanded {
-                          flex: 1.,
-                          tap: move |_| {
-                            if this.cur_idx != idx {
-                              this.cur_idx = idx;
-                            }
-                          },
-                          DynWidget {
-                            h_align: HAlign::Center,
-                            v_align: VAlign::Center,
+        Stack {
+          Row {
+            border: Border::only_bottom(BorderSide {
+              width: 1., color: Palette::of(ctx).primary()
+            }),
+            DynWidget {
+              dyns: {
+                headers.into_iter()
+                  .enumerate()
+                  .map(move |(idx, header)| {
+                    widget! {
+                      Expanded {
+                        id: tab_header,
+                        flex: 1.,
+                        tap: move |_| {
+                          if this.cur_idx != idx {
+                            this.cur_idx = idx;
+                            *active_header_rect = tab_header.layout_rect();
+                          }
+                        },
+                        DynWidget {
+                          h_align: HAlign::Center,
+                          // v_align: VAlign::Center,
 
-                            dyns: header
+                          dyns: header
+                        }
+                      }
+
+                      on tab_header.layout_rect() {
+                        change: move |(_, after)| {
+                          if this.cur_idx == idx {
+                            *active_header_rect = after;
                           }
                         }
                       }
-                    })
-                }
+                    }
+                  })
               }
             }
-            InkBarStyle {
-              id: ink_bar,
-              left_anchor: 0.,
-              top_anchor: 0.,
-              Container {
-                id: ink_box,
-                size: Size::new(0., 0.),
-              }
-            }
+          }
+          InkBarStyle {
+            ink_bar_rect: active_header_rect.clone(),
           }
         }
 
@@ -106,38 +111,6 @@ impl ComposeChild for Tabs {
             })
         }
 
-      }
-
-      on this.cur_idx {
-        change: move |(_, after)| {
-          let width = stack.layout_width();
-          let height = stack.layout_height();
-          let pos = (after as f32) * width / (tab_size as f32);
-          ink_bar.left_anchor = PositionUnit::Pixel(pos);
-          ink_bar.top_anchor = PositionUnit::Pixel(height - 2.);
-        }
-      }
-
-      on stack.layout_width() {
-        change: move |(_, after)| {
-          let width = after / (tab_size as f32);
-          let height = 2.;
-          ink_box.size = Size::new(width, height);
-
-          let pos = (this.cur_idx as f32) * width / (tab_size as f32);
-          ink_bar.left_anchor = PositionUnit::Pixel(pos);
-        }
-      }
-
-      on stack.layout_height() {
-        change: move |(_, after)| {
-          ink_bar.top_anchor = PositionUnit::Pixel(after - 2.);
-        }
-      }
-
-      change_on ink_bar.left_anchor Animate {
-        transition: transitions::EASE_IN.of(ctx),
-        lerp_fn: PositionUnit::lerp_fn(ink_bar.layout_width()),
       }
     }
   }
