4db9e21279cbe1c97a07c3d936811fcb0b215a75
sologeek <liyoushan3@gmail.com>
2023-02-12 17:48:33 +0800

    docs(ribir): ✏️ website footer update info

--- website/src/components/Homepage/HomeFooter.tsx
+++ website/src/components/Homepage/HomeFooter.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Link from '@docusaurus/Link';
 import { useColorMode } from '@docusaurus/theme-common';
+
+import {
+  GitHubLogoIcon,
+} from '@radix-ui/react-icons';
 import clsx from 'clsx';
 
 export default function HomeFooter() {
@@ -8,14 +13,25 @@ export default function HomeFooter() {
   return (
     <footer className="bg-secondary-900">
       <div
-        className={clsx('mx-auto flex max-w-7xl flex-col gap-4 px-10 py-8 lg:flex-row lg:items-center lg:gap-8')}
+        className={clsx('mx-auto flex max-w-7xl flex-col gap-4 px-10 pt-8 pb-1 lg:flex-row lg:items-center lg:gap-8')}
       >
-        <div className="flex-1 text-zinc-400 lg:text-center">
-          <p>
-            The official website uses <a target={"_blank"} href={"https://docs.dyte.io/"}>dyte.io</a> theme, thanks for dyte.io.
-            <br/>
+        <div>
+          <img src={`/logo/${colorMode}.svg`} alt="Logo" className="h-10" />
+        </div>
+        <div className="flex items-center gap-3">
+          <Link href="https://github.com/RibirX/Ribir">
+            <GitHubLogoIcon className="h-6 w-6 text-zinc-400 hover:text-primary" />
+          </Link>
+        </div>
+      </div>
+      <div
+        className={clsx('mx-auto flex max-w-7xl flex-col gap-4 px-10 pt-1 pb-8 lg:flex-row lg:items-center lg:gap-8')}
+      >
+        <div className="flex-1 text-zinc-400 lg:text-left">
+          The official website uses <a target={"_blank"} href={"https://docs.dyte.io/"}>dyte.io</a> theme, thanks for dyte.io.
+        </div>
+        <div className="flex-1 text-zinc-400 lg:text-right">
             Build with RibirX and <span className="emoji">❤️</span>
-          </p>
         </div>
       </div>
     </footer>
