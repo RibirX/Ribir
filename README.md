8df43f327610574f6b77655d9a9683cafe2efea5
wjian23 <w_jian23@163.com>
2023-02-21 21:31:21 +0800

    docs(ribir): ✏️ update counter demo

--- README.md
+++ README.md
@@ -26,8 +26,65 @@ Ribir is a framework for building modern native/wasm cross-platform user interfa
 
 ## At First Glance
 
-[todo] counter demo 
 
+<div style="display:inline-block;">
+<div style="float:left;height:400px;overflow:scroll; ">
+
+``` rust
+use ribir::prelude::*;
+
+fn main() {
+  let w = widget! {
+    init ctx => {
+      let ease_in = transitions::EASE_IN.of(ctx);
+      let headline3_style = TypographyTheme::of(ctx).headline3.text.clone();
+    }
+    states {
+      cnt: Stateful::new(0_i32),
+    }
+    Column {
+      Row {
+        margin: EdgeInsets::all(2.),
+        Button {
+          on_tap: move |_| *cnt += 1,
+          margin: EdgeInsets::only_right(2.),
+          ButtonText::new("Add")
+        }
+        Button {
+          on_tap: move |_| *cnt -= 1,
+          margin: EdgeInsets::only_right(2.),
+          ButtonText::new("Sub")
+        }
+      }
+      Row {
+        Text { text: "current count:" }
+        Text {
+          id: text,
+          text: {
+            let cnt = *cnt;
+            format!("{cnt}")
+          },
+          style: headline3_style,
+        }
+      }
+    }
+    Animate {
+      id: animate,
+      transition: ease_in,
+      prop: prop!(text.transform),
+      from: Transform::translation(0., text.layout_height() * -2.)
+    }
+    finally {
+      let_watch!(*cnt)
+        .subscribe(move |_| animate.run());
+    }
+  };
+  app::run(w);
+}
+```
+</div>
+<img src="website/static/img/counter_demo.gif" style="float:right;"/>
+</div>
 
 More [Examples]
 
--- ribir/examples/counter.rs
+++ ribir/examples/counter.rs
@@ -2,27 +2,48 @@ use ribir::prelude::*;
 
 fn main() {
   let w = widget! {
-    states {
-    cnt: Stateful::new(0_i32),
+    init ctx => {
+      let ease_in = transitions::EASE_IN.of(ctx);
+      let headline3_style = TypographyTheme::of(ctx).headline3.text.clone();
     }
-    Row {
-      margin: EdgeInsets::all(2.),
-      Button {
-        on_tap: move |_| *cnt += 1,
-        margin: EdgeInsets::only_right(2.),
-        ButtonText::new("Add")
-      }
-      Button {
-        on_tap: move |_| *cnt -= 1,
-        margin: EdgeInsets::only_right(2.),
-        ButtonText::new("Sub")
-      }
-      Text {
-        text: {
-          let cnt = *cnt;
-          format!("current count: {cnt}")
+    states {
+      cnt: Stateful::new(0_i32),
+    }
+    Column {
+      Row {
+        margin: EdgeInsets::all(2.),
+        Button {
+          on_tap: move |_| *cnt += 1,
+          margin: EdgeInsets::only_right(2.),
+          ButtonText::new("Add")
+        }
+        Button {
+          on_tap: move |_| *cnt -= 1,
+          margin: EdgeInsets::only_right(2.),
+          ButtonText::new("Sub")
         }
       }
+      Row {
+        Text { text: "current count:" }
+        Text {
+          id: text,
+          text: {
+            let cnt = *cnt;
+            format!("{cnt}")
+          },
+          style: headline3_style,
+        }
+      }
+    }
+    Animate {
+      id: animate,
+      transition: ease_in,
+      prop: prop!(text.transform),
+      from: Transform::translation(0., text.layout_height() * -2.)
+    }
+    finally {
+      let_watch!(*cnt)
+        .subscribe(move |_| animate.run());
     }
   };
   app::run(w);
