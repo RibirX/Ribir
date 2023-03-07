c3680203afd03283516f9c0167a1f783265d69e8
Adoo <Adoo@outlook.com>
2023-03-07 18:18:00 +0800

    feat(ribir): 🎸 add a tests package to write unit test that depends other packages

--- tests/compile_message.rs
+++ tests/compile_message.rs
@@ -1,15 +1,14 @@
-use ribir_macros::include_svg;
+use ribir::prelude::{include_svg, SvgPaths};
 
 #[test]
-fn ui() {
+fn compile_msg() {
   let t = trybuild::TestCases::new();
-  t.compile_fail("tests/ui/**/*fail.rs");
-  t.pass("tests/ui/**/*pass.rs");
+  t.compile_fail("tests/compile_msg/**/*fail.rs");
+  t.pass("tests/compile_msg/**/*pass.rs");
 }
 
 #[test]
 fn include_svg() {
-  use ribir_painter::SvgPaths;
   let svg: SvgPaths = include_svg!("./assets/test1.svg");
   assert_eq!(svg.paths.len(), 2);
 }
