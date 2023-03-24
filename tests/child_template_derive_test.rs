bb88a6f6e361192051d1e5a6e5fbf69c0cb69376
Adoo <Adoo@outlook.com>
2023-03-24 16:24:21 +0800

    feat(macros): 🎸 support derive `Template` for tuple struct

--- tests/child_template_derive_test.rs
+++ tests/child_template_derive_test.rs
@@ -20,6 +20,17 @@ impl ComposeChild for P {
   fn compose_child(_: State<Self>, _: Self::Child) -> Widget { Void.into_widget() }
 }
 
+#[derive(Declare)]
+struct P2;
+#[derive(Template)]
+struct TupleStructTemplate(ChildA, Option<ChildB>, Option<ChildC>);
+
+impl ComposeChild for P2 {
+  type Child = TupleStructTemplate;
+
+  fn compose_child(_: State<Self>, _: Self::Child) -> Widget { Void.into_widget() }
+}
+
 #[test]
 fn syntax_pass() {
   let _no_care_order = widget! {
@@ -33,5 +44,19 @@ fn syntax_pass() {
     P {
       self::ChildA
     }
+  };
+}
+
+#[test]
+fn tuple_struct_template_syntax_pass() {
+  let _no_care_order = widget! {
+    P2 {
+      self::ChildC
+      self::ChildA
+      self::ChildB
+    }
+  };
+  let _omit_option = widget! {
+    P2 { self::ChildA }
   };
 }
