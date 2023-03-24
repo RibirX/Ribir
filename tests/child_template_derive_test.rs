856f81f09e6fe2cbafa75e027c7c1854ec0bb100
Adoo <Adoo@outlook.com>
2023-03-24 18:36:20 +0800

    feat(macros): 🎸 support enum derive `Template`

--- tests/child_template_derive_test.rs
+++ tests/child_template_derive_test.rs
@@ -1,4 +1,4 @@
-use ribir::prelude::*;
+use ribir::{core::test::expect_layout_result, prelude::*};
 
 #[derive(Declare)]
 struct P;
@@ -22,11 +22,28 @@ impl ComposeChild for P {
 
 #[derive(Declare)]
 struct P2;
+
 #[derive(Template)]
 struct TupleStructTemplate(ChildA, Option<ChildB>, Option<ChildC>);
 
 impl ComposeChild for P2 {
   type Child = TupleStructTemplate;
+
+  fn compose_child(_: State<Self>, _: Self::Child) -> Widget { Void.into_widget() }
+}
+
+#[derive(Declare)]
+struct P3;
+
+#[derive(Template)]
+enum EnumTml {
+  A(ChildA),
+  B(ChildB),
+  C(ChildC),
+}
+
+impl ComposeChild for P3 {
+  type Child = EnumTml;
 
   fn compose_child(_: State<Self>, _: Self::Child) -> Widget { Void.into_widget() }
 }
@@ -59,4 +76,29 @@ fn tuple_struct_template_syntax_pass() {
   let _omit_option = widget! {
     P2 { self::ChildA }
   };
+}
+
+#[test]
+fn enum_template() {
+  let _a = widget! {
+    P3 { self::ChildA }
+  };
+  let _b = widget! {
+    P { self::ChildB }
+  };
+  let _c = widget! {
+    P { self::ChildC }
+  };
+}
+
+#[test]
+#[should_panic = "Try to fill enum template with two variant."]
+fn panic_multi_enum_variant() {
+  let a = widget! {
+    P3 {
+      self::ChildA
+      self::ChildB
+    }
+  };
+  expect_layout_result(a, None, &[]);
 }
