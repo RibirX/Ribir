a6cbec67f324cd5829fd2ff863729afefd63a19b
Adoo <Adoo@outlook.com>
2023-03-24 16:24:21 +0800

    feat(macros): 🎸 support derive `Template` for tuple struct

--- macros/src/child_template.rs
+++ macros/src/child_template.rs
@@ -1,8 +1,8 @@
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
 use syn::{
-  parse_quote, punctuated::Punctuated, token::Comma, AngleBracketedGenericArguments, Field, Fields,
-  GenericArgument, PathArguments, PathSegment,
+  parse_quote, punctuated::Pair, token::Comma, AngleBracketedGenericArguments, Field, Fields,
+  FieldsNamed, FieldsUnnamed, GenericArgument, Index, PathArguments, PathSegment,
 };
 
 use crate::util::data_struct_unwrap;
@@ -12,73 +12,15 @@ const ASSOCIATED_TEMPLATE: &str = "AssociatedTemplate";
 pub(crate) fn derive_child_template(input: &mut syn::DeriveInput) -> syn::Result<TokenStream> {
   let syn::DeriveInput { vis, ident: name, generics, data, .. } = input;
   let (g_impl, g_ty, g_where) = generics.split_for_impl();
-
-  let stt = data_struct_unwrap(data, ASSOCIATED_TEMPLATE)?;
   let tml = Ident::new(&format!("{name}{TML}"), name.span());
 
-  let fields = match stt.fields {
-    Fields::Named(ref named) => &named.named,
-    Fields::Unit => {
-      let err_str = format!("Can't derive `{ASSOCIATED_TEMPLATE}` for a empty template.",);
-      return Err(syn::Error::new(Span::call_site(), err_str));
-    }
-    Fields::Unnamed(_) => unreachable!(),
-  };
-
-  let mut init_values = quote! {};
-  let mut fill_child_impl = quote! {};
-  // builder define
-  let builder_fields: Punctuated<Field, Comma> = fields
-    .clone()
-    .into_pairs()
-    .map(|mut p| {
-      let field = p.value();
-      let field_name = field.ident.as_ref();
-      let mut ty = &field.ty;
-
-      let mut value = quote! { self.#field_name };
-      if let Some(inner_ty) = option_type_extract(ty) {
-        ty = inner_ty
-      } else {
-        let err = format!("Required child `{}` not specify", quote! { #ty });
-        value.extend(quote! { .expect(#err)});
-      };
-      let punct = p.punct();
-      init_values.extend(quote! { #field_name: #value #punct});
-
-      fill_child_impl.extend(quote! {
-        impl #g_impl FillTml<SelfImpl, #ty> for #tml #g_ty #g_where  {
-          fn fill(&mut self, c: #ty) {
-            assert!(self.#field_name.is_none(), "Try to fill same type twice.");
-            self.#field_name = Some(c);
-          }
-        }
-      });
-
-      p.value_mut().ty = parse_quote!(Option<#ty>);
-      p
-    })
-    .collect();
-
-  let tokens = quote! {
-    #[derive(Default)]
-    #vis struct #tml #g_ty #g_where {
-      #builder_fields
-    }
-
+  let mut tokens = quote! {
     impl #g_impl Template for #name #g_ty #g_where {
       type Builder = #tml #g_ty;
 
       #[inline]
       fn builder() -> Self::Builder {  <_>::default() }
     }
-
-    impl #g_impl TemplateBuilder for #tml #g_ty #g_where {
-      type Target = #name #g_ty;
-      #[inline]
-      fn build_tml(self) -> Self::Target {#name { #init_values }}
-    }
-
 
     impl #g_impl Declare for #name #g_ty #g_where {
       type Builder = #tml #g_ty;
@@ -91,10 +33,81 @@ pub(crate) fn derive_child_template(input: &mut syn::DeriveInput) -> syn::Result
       #[inline]
       fn build(self, _: &BuildCtx) -> Self { self }
     }
-
-    #fill_child_impl
   };
 
+  let fill_tml_impl = |field_name: TokenStream, ty: &syn::Type, tokens: &mut TokenStream| {
+    tokens.extend(quote! {
+      impl #g_impl FillTml<SelfImpl, #ty> for #tml #g_ty #g_where  {
+        fn fill(&mut self, c: #ty) {
+          assert!(self.#field_name.is_none(), "Try to fill same type twice.");
+          self.#field_name = Some(c);
+        }
+      }
+    });
+  };
+
+  let stt = data_struct_unwrap(data, ASSOCIATED_TEMPLATE)?;
+  match &stt.fields {
+    Fields::Named(FieldsNamed { named: fields, .. }) => {
+      let builder_fields = fields.clone().into_pairs().map(convert_to_builder_pair);
+      tokens.extend(quote! {
+        #[derive(Default)]
+        #vis struct #tml #g_ty #g_where {
+          #(#builder_fields)*
+        }
+      });
+
+      let init_values = fields.iter().map(|field| {
+        let field_name = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+        let value = gen_init_value_tokens(quote!(#field_name), ty);
+        quote! {#field_name: #value}
+      });
+
+      tokens.extend(quote! {
+        impl #g_impl TemplateBuilder for #tml #g_ty #g_where {
+          type Target = #name #g_ty;
+          #[inline]
+          fn build_tml(self) -> Self::Target {#name { #(#init_values),* }}
+        }
+      });
+      fields.iter().for_each(|f| {
+        let f_name = f.ident.as_ref().unwrap();
+        let ty = option_type_extract(&f.ty).unwrap_or(&f.ty);
+        fill_tml_impl(quote! {#f_name}, ty, &mut tokens)
+      });
+    }
+    Fields::Unnamed(FieldsUnnamed { unnamed: fields, .. }) => {
+      let builder_fields = fields.clone().into_pairs().map(convert_to_builder_pair);
+      tokens.extend(quote! {
+        #[derive(Default)]
+        #vis struct #tml #g_ty #g_where(#(#builder_fields)*);
+      });
+
+      let init_values = fields.iter().enumerate().map(|(idx, field)| {
+        let idx = Index::from(idx);
+        gen_init_value_tokens(quote!(#idx), &field.ty)
+      });
+
+      tokens.extend(quote! {
+        impl #g_impl TemplateBuilder for #tml #g_ty #g_where {
+          type Target = #name #g_ty;
+          #[inline]
+          fn build_tml(self) -> Self::Target {#name(#(#init_values),* ) }
+        }
+      });
+
+      fields.iter().enumerate().for_each(|(idx, f)| {
+        let ty = option_type_extract(&f.ty).unwrap_or(&f.ty);
+        let idx = Index::from(idx);
+        fill_tml_impl(quote! {#idx}, ty, &mut tokens)
+      });
+    }
+    Fields::Unit => {
+      let err_str = format!("Can't derive `{ASSOCIATED_TEMPLATE}` for a empty template.",);
+      return Err(syn::Error::new(Span::call_site(), err_str));
+    }
+  };
   Ok(tokens)
 }
 
@@ -128,4 +141,21 @@ fn option_type_extract(ty: &syn::Type) -> Option<&syn::Type> {
       Some(GenericArgument::Type(ref ty)) => Some(ty),
       _ => None,
     })
+}
+
+fn convert_to_builder_pair(mut p: Pair<Field, Comma>) -> Pair<Field, Comma> {
+  let ty = &p.value().ty;
+  if option_type_extract(ty).is_none() {
+    p.value_mut().ty = parse_quote!(Option<#ty>);
+  };
+  p
+}
+
+fn gen_init_value_tokens(field_name: TokenStream, ty: &syn::Type) -> TokenStream {
+  let mut value = quote! { self.#field_name };
+  if option_type_extract(ty).is_none() {
+    let err = format!("Required child `{}` not specify", quote! { #ty });
+    value.extend(quote! { .expect(#err)});
+  };
+  value
 }
