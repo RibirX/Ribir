c677f5f79485ea4484280b26c76fd1051f437a6d
Adoo <Adoo@outlook.com>
2023-04-01 21:57:02 +0800

    test(macros): 💍 fix compile message test not run

--- tests/compile_message.rs
+++ tests/compile_message.rs
@@ -3,8 +3,8 @@ use ribir::prelude::{include_svg, SvgPaths};
 #[test]
 fn compile_msg() {
   let t = trybuild::TestCases::new();
-  t.compile_fail("tests/compile_msg/**/*fail.rs");
-  t.pass("tests/compile_msg/**/*pass.rs");
+  t.compile_fail("./compile_msg/**/*fail.rs");
+  t.pass("./compile_msg/**/*pass.rs");
 }
 
 #[test]
