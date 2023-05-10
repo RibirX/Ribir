871a2595e92d3bcbfe767551b7eedf6c441b3e68
wjian23 <w_jian23@163.com>
2023-05-10 22:22:58 +0800

    feat(geom): 🎸 common geometry type definition

--- text/src/typography.rs
+++ text/src/typography.rs
@@ -1,11 +1,5 @@
+use ribir_geom::{Size, Zero};
 use std::{iter::Peekable, ops::Range};
-
-use lyon_path::geom::euclid::num::Zero;
-use ribir_geom::LogicUnit;
-pub type Rect<T> = lyon_path::geom::euclid::Rect<T, LogicUnit>;
-pub type Point<T> = lyon_path::geom::euclid::Point2D<T, LogicUnit>;
-pub type Size<T> = lyon_path::geom::euclid::Size2D<T, LogicUnit>;
-
 use unicode_script::{Script, UnicodeScript};
 
 use crate::{Em, FontSize, Glyph, Pixel, TextAlign};
