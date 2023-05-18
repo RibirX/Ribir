310ba4d6da7c65f027139f91f032092441cff52c
Adoo <Adoo@outlook.com>
2023-05-18 13:55:46 +0800

    refactor(core): 💡 remove test harness `unit_test_describe` to `ribir_dev_helper`

--- core/src/test.rs
+++ core/src/test.rs
@@ -237,12 +237,6 @@ impl Window {
   pub fn widget_count(&self) -> usize { self.widget_tree.count() }
 }
 
-#[allow(unused)]
-macro count {
-  () => (0usize),
-  ( $x:tt $($xs:tt)* ) => (1usize + count!($($xs)*))
-}
-
 impl ExpectRect {
   pub fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
     Self {
@@ -278,80 +272,3 @@ impl ExpectRect {
     }
   }
 }
-
-/// A unit test help macro to describe the test flow. This macro provide ability
-/// to pack many unit tests, and print the result like official. Should always
-/// use official test harness first, use it only when you need execute test by
-/// self.
-///
-/// This macro depends on crate `colored`.
-///
-/// # Example
-///
-/// ```
-/// use ribir_core::test::unit_test_describe;
-///
-/// fn test_first() {}
-///
-/// fn test_second() {}
-///
-/// fn main() {
-///   use colored::Colorize;
-///
-///   unit_test_describe!{
-///     run_unit_test(test_first);
-///     run_unit_test(test_second);
-///   }
-/// }
-/// ```
-
-pub macro unit_test_describe($(run_unit_test($name: path);)* ) {{
-  let panic_infos: std::sync::Arc<std::sync::Mutex<Vec<String>>> = Default::default();
-
-  // hook panic to format message
-  let c_infos = panic_infos.clone();
-  std::panic::set_hook(Box::new(move |info| {
-    println!("... {}", "failed".red());
-    let info_str = format!("{}", info);
-    c_infos.lock().unwrap().push(info_str)
-  }));
-
-  let count = count!($($name)*);
-
-  println!("running {} test", count);
-  let mut res  = Result::Ok(());
-  // catch panic and continue execute unit tests.
-  $(
-   res = std::panic::catch_unwind(|| {
-      // run the unit tests
-      print!("test {}::{} ", module_path!(), stringify!($name));
-      $name();
-      println!("... {}", "ok".green());
-    }).and(res);
-  )*
-
-  // remove panic hook
-  let _ = std::panic::take_hook();
-
-  // unit tests result message.
-  let infos = panic_infos.lock().unwrap();
-  let failed = infos.len();
-  let pass = count - failed;
-  let result = if res.is_err() {
-    "failed".red()
-  } else {
-    "ok".green()
-  };
-  println!("");
-  println!("test results: {}. {} passed; {} failed;\n", result, pass, failed);
-
-  if !infos.is_empty() {
-    println!("--------- {} failed stdout ---------", module_path!());
-    infos.iter().for_each(|info| println!("{}", info))
-  }
-
-
-  if let Result::Err(err) = res {
-    std::panic::resume_unwind(err);
-  }
-}}
--- dev-helper/src/lib.rs
+++ dev-helper/src/lib.rs
@@ -1,2 +1,3 @@
 mod painter_backend_eq_image_test;
 pub use painter_backend_eq_image_test::*;
+mod unit_test_describe;
