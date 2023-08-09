5f8d810ff7c6f427fca57cc1365c046e0e95dc89
Adoo <Adoo@outlook.com>
2023-08-09 21:27:16 +0800

    feat(core): 🎸 support system hotkey for OSX

--- ribir/src/platform/macos.rs
+++ ribir/src/platform/macos.rs
@@ -1,14 +1,16 @@
 use crate::prelude::{App, AppEvent};
-use std::sync::Once;
-
-use objc2::{
-  declare_class, extern_class, extern_methods,
-  foundation::NSObject,
-  msg_send, msg_send_id,
-  rc::{Id, Owned, Shared},
-  runtime::{Object, Sel},
-  sel, ClassType,
+use icrate::{
+  block2::ConcreteBlock,
+  objc2::{
+    rc::Id,
+    runtime::{AnyObject, Sel},
+    *,
+  },
+  AppKit::{NSEvent, NSEventMaskKeyDown, NSEventMaskLeftMouseDown},
+  Foundation::NSObject,
 };
+
+use std::{ptr::NonNull, sync::Once};
 
 extern_class!(
   #[derive(Debug, PartialEq, Eq, Hash)]
@@ -16,22 +18,24 @@ extern_class!(
 
   unsafe impl ClassType for NSAppleEventManager {
     type Super = NSObject;
+    type Mutability = mutability::Mutable;
+    const NAME: &'static str = "NSAppleEventManager";
   }
 );
 
 extern_methods!(
   unsafe impl NSAppleEventManager {
-    fn shared() -> Id<Self, Shared> {
+    fn shared() -> Id<Self> {
       unsafe {
         let manager: Option<_> = msg_send_id![Self::class(), sharedAppleEventManager];
         manager.unwrap_unchecked()
       }
     }
 
-    #[sel(setEventHandler:andSelector:forEventClass:andEventID:)]
+    #[method(setEventHandler:andSelector:forEventClass:andEventID:)]
     fn set_event_handler(
       &self,
-      handler: &Object,
+      handler: &AnyObject,
       and_selector: Sel,
       for_event_class: u32,
       and_event_id: u32,
@@ -44,19 +48,22 @@ declare_class!(
 
   unsafe impl ClassType for AppEventsHandler {
     type Super = NSObject;
+    type Mutability = mutability::Mutable;
+    const NAME: &'static str = "AppEventsHandler";
   }
 
   unsafe impl AppEventsHandler {
-    #[sel(openUrl:withReplyEvent:)]
-    fn open_url(&self, event: &Object, _with_reply_event: &Object) {
+    #[method(openUrl:withReplyEvent:)]
+    fn open_url(&self, event: &NSEvent, _with_reply_event: &AnyObject) {
       let url = unsafe {
         let class: u32 = msg_send![event, eventClass];
         let id: u32 = msg_send![event, eventID];
+
         if class != kInternetEventClass || id != kAEGetURL {
           return;
         }
-        let event: *mut Object = msg_send![event, paramDescriptorForKeyword: keyDirectObject];
-        let nsstring: *mut Object = msg_send![event, stringValue];
+        let event: *mut AnyObject = msg_send![event, paramDescriptorForKeyword: keyDirectObject];
+        let nsstring: *mut AnyObject = msg_send![event, stringValue];
         let cstr: *const i8 = msg_send![nsstring, UTF8String];
         if cstr != std::ptr::null() {
           std::ffi::CStr::from_ptr(cstr)
@@ -81,11 +88,11 @@ pub const kAEGetURL: u32 = 0x4755524c;
 pub const keyDirectObject: u32 = 0x2d2d2d2d;
 
 pub fn register_platform_app_events_handlers() {
-  static mut APP_EVENTS_HANDLERS: Option<Id<AppEventsHandler, Owned>> = None;
+  static mut APP_EVENTS_HANDLERS: Option<Id<AppEventsHandler>> = None;
   static INIT: Once = Once::new();
   unsafe {
     INIT.call_once(|| {
-      let handler: Id<AppEventsHandler, Owned> = msg_send_id![AppEventsHandler::class(), new];
+      let handler: Id<AppEventsHandler> = msg_send_id![AppEventsHandler::class(), new];
       let manager = NSAppleEventManager::shared();
       manager.set_event_handler(
         &handler,
@@ -94,6 +101,16 @@ pub fn register_platform_app_events_handlers() {
         kAEGetURL,
       );
       APP_EVENTS_HANDLERS = Some(handler);
+
+      NSEvent::addGlobalMonitorForEventsMatchingMask_handler(
+        NSEventMaskLeftMouseDown | NSEventMaskKeyDown,
+        &ConcreteBlock::new(|e: NonNull<NSEvent>| {
+          println!("mouse or key down");
+          if let Some(chars) = e.as_ref().characters() {
+            println!("chars: {:?}", chars);
+          }
+        }),
+      );
     });
   }
 }
