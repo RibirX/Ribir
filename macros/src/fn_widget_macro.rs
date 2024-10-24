ee0396ee339f2b541a644c1f435b43289f78b077
Adoo <Adoo@outlook.com>
2024-10-24 21:54:03 +0800

    feat(macros): 🎸 the `part_writer!` macro
    Creating a writer from a mutable reference of a writer will
    automatically switch to the built-in widget if its reference is a
    built-in field or method.

--- macros/src/fn_widget_macro.rs
+++ macros/src/fn_widget_macro.rs
@@ -9,22 +9,21 @@ use crate::{
 };
 
 pub(crate) fn gen_code(input: TokenStream, refs_ctx: &mut DollarRefsCtx) -> TokenStream {
-  let res = symbol_to_macro(input)
-    .and_then(|input| syn::parse2::<BodyExpr>(input).map_err(Into::into))
-    .map(|body| {
-      refs_ctx.new_dollar_scope(None);
-      let stmts: Vec<_> = body
-        .0
-        .into_iter()
-        .map(|s| refs_ctx.fold_stmt(s))
-        .collect();
+  let res = symbol_to_macro(input).and_then(|input| {
+    let body = syn::parse2::<BodyExpr>(input)?;
+    refs_ctx.new_dollar_scope(None);
+    let stmts: Vec<_> = body
+      .0
+      .into_iter()
+      .map(|s| refs_ctx.fold_stmt(s))
+      .collect();
 
-      refs_ctx.current_dollar_scope_mut().used_ctx = false;
-      let _ = refs_ctx.pop_dollar_scope(false);
-      quote! {
-        move |ctx!(): &mut BuildCtx| -> Widget { #(#stmts)*.into_widget() }
-      }
-    });
+    refs_ctx.current_dollar_scope_mut().used_ctx = false;
+    let _ = refs_ctx.pop_dollar_scope(false);
+    Ok(quote! {
+      move |ctx!(): &mut BuildCtx| -> Widget { #(#stmts)*.into_widget() }
+    })
+  });
 
   result_to_token_stream(res)
 }
