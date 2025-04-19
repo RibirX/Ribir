0888a7ffd77a3f8d0bdbe3da5db389d773b7f749
wjian23 <w_jian23@163.com>
2025-04-19 09:38:57 +0800

    fix(core): 🐛 Fixed Reuseable panic when when holding the same Reuseable while pipe changes continuously.

--- core/src/reusable.rs
+++ core/src/reusable.rs
@@ -38,10 +38,21 @@ pub struct Reusable(Sc<RefCell<ReusableState>>);
 impl Reusable {
   /// Creates a new Reusable container from a widget.
   ///
-  /// Use `get_widget()` to retrieve a widget instance.
-  pub fn new<const M: usize>(mut w: FatObj<impl IntoWidget<'static, M>>) -> Self {
-    let track_id = w.get_track_id_widget().read().track_id();
-    Self(Sc::new(RefCell::new(ReusableState::Init { track_id, widget: w.into_widget() })))
+  /// returns`(Widget, Reusable)`, the Widget must be placed before the Reusable
+  /// being used again, otherwise it will panic.
+  pub fn new<'a, const M: usize>(
+    w: impl IntoWidget<'a, M>, wnd: &Sc<Window>,
+  ) -> (Widget<'a>, Self) {
+    let mut obj = FatObj::new(w);
+    let track_id = obj.get_track_id_widget().read().track_id();
+    let this = Self(Sc::new(RefCell::new(ReusableState::Recycled { track_id, wnd_id: wnd.id() })));
+
+    (this.handle_reusable_wrapper(obj.into_widget()), this)
+  }
+
+  pub fn is_in_used(&self) -> bool {
+    let inner_state = self.0.borrow();
+    matches!(*inner_state, ReusableState::Using { .. })
   }
 
   /// Retrieves a widget instance for placement in the UI.
@@ -54,34 +65,50 @@ impl Reusable {
   /// - If the widget is already active in the UI
   /// - If called after `release()` or during window disposal
   /// - When crossing window boundaries
-  pub fn get_widget(&mut self) -> FatObj<Widget<'static>> {
-    let (w, track_id) = {
-      let inner_state = &mut *self.0.borrow_mut();
-      match inner_state {
-        ReusableState::Init { track_id, widget: w } => {
-          (std::mem::replace(w, Void.into_widget()), track_id.clone())
-        }
-        ReusableState::Recycled { track_id, wnd_id } => {
-          let wnd_id = *wnd_id;
-          let w = Widget::from_fn({
-            let track_id = track_id.clone();
-            move |ctx| {
-              let current_wnd_id = ctx.window().id();
-              assert_eq!(
-                wnd_id, current_wnd_id,
-                "Reusable widget must be used in the same window it was recycled in."
-              );
-              track_id.get().unwrap()
-            }
-          });
-          (w, track_id.clone())
-        }
-        ReusableState::Building(_) | ReusableState::Dropped => {
-          panic!("Widget in invalid state for reuse. Expected Init/Recycled")
-        }
+  pub fn get_widget(&self) -> Widget<'static> {
+    let mut this = self.clone();
+    fn_widget! {
+      this.gen_widget()
+    }
+    .into_widget()
+  }
+
+  // The widget is generated lazily when `get_widget` is called, with actual
+  // instantiation deferred until needed via `gen_widget`.
+  fn gen_widget(&mut self) -> Widget<'static> {
+    let mut inner_state = self.0.borrow_mut();
+    let w = match &mut *inner_state {
+      ReusableState::Using { render, track_id } => {
+        let render = render.clone();
+        let track_id = track_id.clone();
+        Widget::from_fn(move |ctx| {
+          let reuse_id =
+            move_inner_render_to_new(render.take(), track_id.get().unwrap(), ctx.tree_mut());
+          reuse_id
+        })
+      }
+      ReusableState::Recycled { track_id, wnd_id } => {
+        let wnd_id = *wnd_id;
+        Widget::from_fn({
+          let track_id = track_id.clone();
+          move |ctx| {
+            let current_wnd_id = ctx.window().id();
+            assert_eq!(
+              wnd_id, current_wnd_id,
+              "Reusable widget must be used in the same window it was recycled in."
+            );
+            track_id
+              .get()
+              .expect("reusable has no be used yet")
+          }
+        })
+      }
+      ReusableState::Dropped => {
+        panic!("Widget in invalid state for reuse. Expected Init/Recycled")
       }
     };
-    self.handle_reusable_wrapper(track_id, w)
+
+    self.handle_reusable_wrapper(w)
   }
 
   /// Permanently disposes of the managed widget and associated resources.
@@ -92,62 +119,80 @@ impl Reusable {
     let mut inner_state = self.0.borrow_mut();
     if let ReusableState::Recycled { track_id, wnd_id } = &*inner_state {
       let wid = track_id.get().unwrap();
-      let wnd = AppCtx::get_window(*wnd_id).unwrap_or_else(|| {
-        panic!("Window for reusable widget already closed");
-      });
-      wid.dispose_subtree(wnd.tree_mut());
+      if let Some(wnd) = AppCtx::get_window(*wnd_id) {
+        wid.dispose_subtree(wnd.tree_mut());
+      }
     }
     *inner_state = ReusableState::Dropped;
   }
 
-  fn handle_reusable_wrapper(
-    &self, track_id: TrackId, widget: Widget<'static>,
-  ) -> FatObj<Widget<'static>> {
-    let render_cell = ReusableRenderWrapper::default();
-    let widget = widget.on_build({
-      let render_cell = render_cell.clone();
-      move |id| {
-        id.wrap_node(BuildCtx::get_mut().tree_mut(), |render_node| {
-          render_cell.set(render_node);
-          Box::new(render_cell.clone())
-        })
-      }
-    });
-
+  fn handle_reusable_wrapper<'a>(&self, widget: Widget<'a>) -> Widget<'a> {
     let mut fat = FatObj::new(widget);
     let this = self.clone();
     fat.on_disposed(move |e| {
-      // We will recycle the original widget, so we prevent to continue emitting
-      // the `Disposed` event to the original widget.
-      e.prevent_default();
+      if matches!(*this.0.borrow(), ReusableState::Using { .. }) {
+        // We will recycle the original widget, so we prevent to continue emitting
+        // the `Disposed` event to the original widget.
+        let (render_cell, track_id) = match &*this.0.borrow() {
+          ReusableState::Using { render, track_id, .. } => (render.clone(), track_id.clone()),
+          _ => panic!("Widget in invalid state for reuse. Expected Using"),
+        };
+        if track_id.get() != Some(e.current_target()) {
+          return;
+        }
 
-      let wnd = e.window();
-      let tree = wnd.tree_mut();
-      let id = e.current_target();
+        e.prevent_default();
 
-      let original = render_cell.take();
-      let reuse_id = tree.alloc_node(original);
+        let wnd = e.window();
+        let tree = wnd.tree_mut();
+        let id = e.current_target();
 
-      id.children(tree)
-        .collect::<SmallVec<[WidgetId; 1]>>()
-        .into_iter()
-        .for_each(|child| reuse_id.append(child, tree));
+        move_inner_render_to_new(render_cell.take(), id, tree);
 
-      reuse_id
-        .get_node_mut(tree)
-        .unwrap()
-        .update_track_id(reuse_id);
-
-      let mut inner_state = this.0.borrow_mut();
-      let ReusableState::Building(track_id) = &*inner_state else {
-        panic!("Cannot recycle widget in a wrong state.");
-      };
-
-      *inner_state = ReusableState::Recycled { track_id: track_id.clone(), wnd_id: wnd.id() };
+        *this.0.borrow_mut() =
+          ReusableState::Recycled { track_id: track_id.clone(), wnd_id: wnd.id() };
+      }
     });
-    *self.0.borrow_mut() = ReusableState::Building(track_id);
+    let this = self.clone();
     fat
+      .into_widget()
+      .on_build(move |id| this.on_build(id))
   }
+
+  fn on_build(&self, id: WidgetId) {
+    let track_id = match &*self.0.borrow() {
+      ReusableState::Using { track_id, .. } | ReusableState::Recycled { track_id, .. } => {
+        track_id.clone()
+      }
+      _ => panic!("Widget in invalid state for reuse. Expected Init/Recycled"),
+    };
+
+    let render = ReusableRenderWrapper::default();
+    id.wrap_node(BuildCtx::get_mut().tree_mut(), |render_node| {
+      render.set(render_node);
+      Box::new(render.clone())
+    });
+
+    *self.0.borrow_mut() = ReusableState::Using { track_id, render };
+  }
+}
+
+fn move_inner_render_to_new(
+  inner: Box<dyn RenderQueryable>, origin: WidgetId, tree: &mut WidgetTree,
+) -> WidgetId {
+  let new_id = tree.alloc_node(inner);
+  origin
+    .children(tree)
+    .collect::<SmallVec<[WidgetId; 1]>>()
+    .into_iter()
+    .for_each(|child| new_id.append(child, tree));
+
+  new_id
+    .get_node_mut(tree)
+    .unwrap()
+    .update_track_id(new_id);
+
+  new_id
 }
 
 /// A wrapper around a render node that is used to split the original render
@@ -157,8 +202,7 @@ impl Reusable {
 struct ReusableRenderWrapper(Sc<UnsafeCell<Box<dyn RenderQueryable>>>);
 
 enum ReusableState {
-  Init { track_id: TrackId, widget: Widget<'static> },
-  Building(TrackId),
+  Using { track_id: TrackId, render: ReusableRenderWrapper },
   Recycled { track_id: TrackId, wnd_id: WindowId },
   Dropped,
 }
@@ -223,23 +267,31 @@ mod tests {
     reset_test_env!();
 
     let (info, w_info) = split_value(vec![]);
-    let mut reusable = Reusable::new(FatObj::new(text! {
-      text: "Hello",
-      on_mounted: move |_| $w_info.write().push("Mounted"),
-      on_disposed: move |_| $w_info.write().push("Disposed"),
-    }));
-
-    let (w_trigger, trigger) = split_value(true);
-    let reusable2 = reusable.clone();
+    let (w_trigger, trigger) = split_value(0);
+    let ctrl = Sc::new(RefCell::new(None));
+    let ctrl2 = ctrl.clone();
     let mut wnd = TestWindow::new(fn_widget! {
-      let reusable = reusable2.clone();
+      let (w, reusable) = Reusable::new(@Text {
+        text: "Hello",
+        on_mounted: move |_| $w_info.write().push("Mounted"),
+        on_disposed: move |_| $w_info.write().push("Disposed"),
+      }, &BuildCtx::get().window());
+      *ctrl2.borrow_mut() = Some(reusable.clone());
+      let mut w = Some(w.into_widget());
+      let f = GenWidget::new(move || {
+        if w.is_some() {
+          w.take().unwrap()
+        } else {
+          reusable.get_widget()
+        }
+      });
       @ pipe!{
-        let mut reusable = reusable.clone();
+        let f = f.clone();
         fn_widget! {
-          if *$w_trigger {
-            reusable.get_widget().into_widget()
+          if *$w_trigger < 3 {
+            f.gen_widget()
           } else {
-            Void.into_widget()
+            Void {}.into_widget()
           }
         }
       }
@@ -248,17 +300,19 @@ mod tests {
     wnd.draw_frame();
     assert_eq!(*info.read(), vec!["Mounted"]);
 
-    *trigger.write() = false;
+    *trigger.write() += 1;
     wnd.draw_frame();
     assert_eq!(*info.read(), vec!["Mounted"]);
 
-    *trigger.write() = true;
+    *trigger.write() += 1;
     wnd.draw_frame();
     assert_eq!(*info.read(), vec!["Mounted"]);
 
-    *trigger.write() = false;
+    *trigger.write() += 1;
     wnd.draw_frame();
-    reusable.release();
+    assert_eq!(*info.read(), vec!["Mounted"]);
+
+    ctrl.borrow_mut().as_mut().unwrap().release();
     wnd.draw_frame();
     assert_eq!(*info.read(), vec!["Mounted", "Disposed"]);
   }
