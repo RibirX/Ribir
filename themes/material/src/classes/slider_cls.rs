10eafb7427ef62cbdeefdb8f995452c006013309
Adoo <Adoo@outlook.com>
2026-01-12 14:46:31 +0800

    fix(widgets): 🐛 support click-to-update for slider and refactor layout
    - Update `PointerSelectRegion` to trigger selection start on pointer down.
    - Refactor `Slider` and `RangeSlider` to use `InParentLayout` for indicators, separating them from the track layout.
    - Update material theme definitions and update visual tests.

--- themes/material/src/classes/slider_cls.rs
+++ themes/material/src/classes/slider_cls.rs
@@ -3,37 +3,29 @@ use ribir_widgets::prelude::*;
 
 use crate::md;
 
-const INDICATOR_HEIGHT: f32 = 44.;
+const THUMB_HEIGHT: f32 = 44.;
 const TRACK_HEIGHT: f32 = 16.;
 
 const RADIUS_L2_R8: Radius = Radius::new(2., 8., 2., 8.);
 const RADIUS_L8_R2: Radius = Radius::new(8., 2., 8., 2.);
-
-macro_rules! stop_indicator_class {
-  ($($field: ident: $value: expr),* ) => {
-    style_class! {
-      v_align: VAlign::Center,
-      radius: md::RADIUS_2,
-      margin: md::EDGES_HOR_6,
-      clamp: BoxClamp::fixed_size(md::SIZE_4),
-      $($field: $value),*
-    }
-  };
-}
 
 pub(super) fn init(classes: &mut Classes) {
   classes.insert(
     SLIDER_CONTAINER,
     style_class! {
       cursor: CursorIcon::Pointer,
-      clamp: BoxClamp::fixed_height(INDICATOR_HEIGHT)
+      clamp: BoxClamp::fixed_height(THUMB_HEIGHT)
     },
   );
+
+  classes.insert(SLIDER_THUMB_CONTAINER, style_class! { margin: md::EDGES_HOR_4 });
+
   classes.insert(
     SLIDER_ACTIVE_TRACK,
     style_class! {
       background: BuildCtx::color(),
       radius: RADIUS_L8_R2,
+      margin: md::EDGES_RIGHT_8,
       clamp: BoxClamp::fixed_height(TRACK_HEIGHT),
     },
   );
@@ -41,20 +33,20 @@ pub(super) fn init(classes: &mut Classes) {
   classes.insert(
     SLIDER_INACTIVE_TRACK,
     style_class! {
-      radius: RADIUS_L2_R8,
       background: BuildCtx::container_color(),
+      radius: RADIUS_L2_R8,
+      margin: md::EDGES_LEFT_8,
       clamp: BoxClamp::fixed_height(TRACK_HEIGHT),
     },
   );
 
   classes.insert(
-    SLIDER_INDICATOR,
+    SLIDER_THUMB,
     style_class! {
       v_align: VAlign::Center,
       background: BuildCtx::color(),
       radius: md::RADIUS_2,
-      margin: EdgeInsets::horizontal(6.),
-      clamp: BoxClamp::fixed_size(Size::new(md::THICKNESS_4, INDICATOR_HEIGHT)),
+      clamp: BoxClamp::fixed_size(Size::new(md::THICKNESS_4, THUMB_HEIGHT)),
     },
   );
 
@@ -62,6 +54,7 @@ pub(super) fn init(classes: &mut Classes) {
     RANGE_SLIDER_INACTIVE_TRACK_LEFT,
     style_class! {
       radius: RADIUS_L8_R2,
+      margin: md::EDGES_RIGHT_8,
       background: BuildCtx::container_color(),
       clamp: BoxClamp::fixed_height(TRACK_HEIGHT),
     },
@@ -71,6 +64,7 @@ pub(super) fn init(classes: &mut Classes) {
     RANGE_SLIDER_INACTIVE_TRACK_RIGHT,
     style_class! {
       radius: RADIUS_L2_R8,
+      margin: md::EDGES_LEFT_8,
       background: BuildCtx::container_color(),
       clamp: BoxClamp::fixed_height(TRACK_HEIGHT),
     },
@@ -80,22 +74,35 @@ pub(super) fn init(classes: &mut Classes) {
     RANGE_SLIDER_ACTIVE_TRACK,
     style_class! {
       radius: md::RADIUS_2,
+      margin: md::EDGES_HOR_8,
       background: BuildCtx::color(),
       clamp: BoxClamp::fixed_height(TRACK_HEIGHT),
     },
   );
 
+  named_style_impl! { base_tick => {
+    v_align: VAlign::Center,
+    radius: md::RADIUS_2,
+    clamp: BoxClamp::fixed_size(md::SIZE_4),
+  } }
+
   classes.insert(
-    STOP_INDICATOR_ACTIVE,
-    stop_indicator_class! {
-      background: BuildCtx::color().on_this_color(BuildCtx::get())
-    },
+    SLIDER_TICK_ACTIVE,
+    class_multi_impl![
+      style_class! {
+        background: BuildCtx::color().on_this_color(BuildCtx::get())
+      },
+      base_tick
+    ],
   );
 
   classes.insert(
-    STOP_INDICATOR_INACTIVE,
-    stop_indicator_class! {
-      background: BuildCtx::color().on_this_container_color(BuildCtx::get())
-    },
+    SLIDER_TICK_INACTIVE,
+    class_multi_impl![
+      style_class! {
+        background: BuildCtx::color().on_this_container_color(BuildCtx::get())
+      },
+      base_tick
+    ],
   );
 }
--- widgets/src/slider.rs
+++ widgets/src/slider.rs
@@ -5,8 +5,10 @@ use crate::prelude::*;
 class_names! {
   #[doc = "Class name for the slider container"]
   SLIDER_CONTAINER,
-  #[doc = "Class name for the slider indicator"]
-  SLIDER_INDICATOR,
+  #[doc = "Class name for the slider thumb container"]
+  SLIDER_THUMB_CONTAINER,
+  #[doc = "Class name for the slider thumb"]
+  SLIDER_THUMB,
   #[doc = "Class name for the slider track"]
   SLIDER_ACTIVE_TRACK,
   #[doc = "Class name for the slider inactive track"]
@@ -17,10 +19,10 @@ class_names! {
   RANGE_SLIDER_INACTIVE_TRACK_RIGHT,
   #[doc = "Class name for the active track of range slider"]
   RANGE_SLIDER_ACTIVE_TRACK,
-  #[doc="Class name for the active stop indicator"]
-  STOP_INDICATOR_ACTIVE,
-  #[doc="Class name for the inactive stop indicator"]
-  STOP_INDICATOR_INACTIVE,
+  #[doc="Class name for the active tick mark"]
+  SLIDER_TICK_ACTIVE,
+  #[doc="Class name for the inactive tick mark"]
+  SLIDER_TICK_INACTIVE,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -44,6 +46,13 @@ pub struct RangeSliderChanged {
 }
 
 pub type RangeSliderChangedEvent = CustomEvent<RangeSliderChanged>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum SliderTicks {
+  #[default]
+  None,
+  Always,
+}
 
 trait SliderCore {
   fn min(&self) -> f32;
@@ -124,6 +133,28 @@ fn slider_tooltip(min: f32, max: f32, val: f32) -> String {
   let precision = ((max - min).log10().floor() - 2.).min(-2.).abs() as usize;
   format!("{:.1$}", val, precision)
 }
+
+fn slider_ticks(
+  divisions: usize, range: std::ops::RangeInclusive<usize>,
+  is_hide: impl Fn(usize) -> bool + 'static,
+) -> Widget<'static> {
+  fn_widget! {
+    @Flex {
+      v_align: VAlign::Center,
+      align_items: Align::Center,
+      justify_content: JustifyContent::SpaceBetween,
+      @ {
+        (0..=divisions).map(move |i| {
+          @Void {
+            class: if range.contains(&i) { SLIDER_TICK_ACTIVE } else { SLIDER_TICK_INACTIVE },
+            opacity: if is_hide(i) { 0. } else { 1. },
+          }
+        })
+      }
+    }
+  }
+  .into_widget()
+}
 /// The widget displays a slider.
 #[declare(validate)]
 pub struct Slider {
@@ -192,20 +223,51 @@ impl Slider {
     self.set_value(self.value);
   }
 
-  fn stop_indicator_track(&self) -> Option<BoxFnWidget<'static>> {
-    let divisions = self.divisions?;
-    let active = (self.ratio() * divisions as f32).round() as usize;
-    Some(stop_indicator_track(divisions + 1, 0..=active, vec![active]))
+  fn thumb_container(this: impl StateWriter<Value = Self>) -> Widget<'static> {
+    fn_widget! {
+      let ticks = Provider::of::<SliderTicks>(BuildCtx::get()).map(|v| *v).unwrap_or_default();
+      let mut thumb_container = @Stack { class: SLIDER_THUMB_CONTAINER };
+      let thumb_container_width = thumb_container.layout_width();
+      let mut thumb = @Void {
+        class: SLIDER_THUMB,
+        tooltips: pipe! {
+          let this = $read(this);
+          slider_tooltip(this.min, this.max, this.value)
+        },
+      };
+      let thumb_width = thumb.layout_width();
+      @ (thumb_container) {
+        h_align: HAlign::Stretch,
+        @pipe! {
+          (ticks == SliderTicks::Always).then(|| {
+            let this = $read(this);
+            let divisions = this.divisions?;
+            let active = (this.ratio() * divisions as f32).round() as usize;
+            Some(slider_ticks(divisions, 0..=active, move |i| i == active))
+          }).flatten()
+        }
+        @(thumb) {
+          anchor: pipe! {
+            let ratio = $read(this).ratio();
+            let track_width = *$read(thumb_container_width);
+            let thumb_width = *$read(thumb_width);
+            Anchor::left(ratio * track_width - thumb_width / 2.)
+          }
+        }
+      }
+    }
+    .into_widget()
   }
 }
 
 impl Compose for Slider {
   fn compose(this: impl StateWriter<Value = Self>) -> Widget<'static> {
     fn_widget! {
-      let mut row = @Flex { align_items: Align::Center };
+      let mut track = @Flex { align_items: Align::Center };
+      let track_width = track.layout_width();
       @PointerSelectRegion {
         on_custom: move |e: &mut PointerSelectEvent| {
-          let width = *$read(row.layout_width());
+          let width = *$read(track_width);
           if width > 0. {
             let (_, to) = e.data().endpoints();
             slider_update(&$writer(this), e, to.x / width);
@@ -213,25 +275,20 @@ impl Compose for Slider {
         },
         @Stack {
           class: SLIDER_CONTAINER,
-          @(row) {
+          @(track) {
             v_align: VAlign::Center,
             @Expanded {
               flex: pipe!($read(this).ratio()),
               @Void { class: SLIDER_ACTIVE_TRACK }
-            }
-            @Void {
-              class: SLIDER_INDICATOR,
-              tooltips: pipe! {
-                let this = $read(this);
-                slider_tooltip(this.min, this.max, this.value)
-              },
             }
             @Expanded {
               flex: pipe!(1. - $read(this).ratio()),
               @Void { class: SLIDER_INACTIVE_TRACK }
             }
           }
-          @{ pipe!($read(this).stop_indicator_track() ) }
+          @InParentLayout {
+            @Slider::thumb_container($writer(this))
+          }
         }
       }
     }
@@ -277,12 +334,61 @@ impl RangeSlider {
     }
     Ok(self)
   }
-}
 
-impl SliderCore for RangeSlider {
-  fn min(&self) -> f32 { self.min }
-  fn max(&self) -> f32 { self.max }
-  fn divisions(&self) -> Option<usize> { self.divisions }
+  fn thumb_container(this: impl StateWriter<Value = Self>) -> Widget<'static> {
+    fn_widget! {
+      let ticks = Provider::of::<SliderTicks>(BuildCtx::get()).map(|v| *v).unwrap_or_default();
+      let mut thumb_container = @Stack { class: SLIDER_THUMB_CONTAINER };
+      let thumb_container_width = thumb_container.layout_width();
+
+      let mut start_thumb = @Void {
+        class: SLIDER_THUMB,
+        tooltips: pipe! {
+          let this = $read(this);
+          slider_tooltip(this.min, this.max, this.start)
+        },
+      };
+      let start_thumb_width = start_thumb.layout_width();
+      let mut end_thumb = @Void {
+        class: SLIDER_THUMB,
+        tooltips: pipe! {
+          let this = $read(this);
+          slider_tooltip(this.min, this.max, this.end)
+        },
+      };
+      let end_thumb_width = end_thumb.layout_width();
+
+      @(thumb_container) {
+        h_align: HAlign::Stretch,
+        @pipe! {
+          (ticks == SliderTicks::Always).then(|| {
+            let this = $read(this);
+            let divisions = this.divisions?;
+            let start = (this.start_ratio() * divisions as f32).round() as usize;
+            let end = (this.end_ratio() * divisions as f32).round() as usize;
+            Some(slider_ticks(divisions, start..=end, move |i| i == start || i == end))
+          }).flatten()
+        }
+        @(start_thumb) {
+          anchor: pipe! {
+            let ratio = $read(this).start_ratio();
+            let track_width = *$read(thumb_container_width);
+            let thumb_width = *$read(start_thumb_width);
+            Anchor::left(ratio * track_width - thumb_width / 2.)
+          }
+        }
+        @(end_thumb) {
+          anchor: pipe! {
+            let ratio = $read(this).end_ratio();
+            let track_width = *$read(thumb_container_width);
+            let thumb_width = *$read(end_thumb_width);
+            Anchor::left(ratio * track_width - thumb_width / 2.)
+          }
+        }
+      }
+    }
+    .into_widget()
+  }
 }
 
 impl RangeSlider {
@@ -297,27 +403,6 @@ impl RangeSlider {
   pub fn divisions(&self) -> Option<usize> { self.divisions }
 
   pub fn value(&self) -> RangeSliderValue { RangeSliderValue { start: self.start, end: self.end } }
-
-  fn choose_part(&self, ratio: f32) -> RangeSliderPart {
-    let val = self.snap_v(self.convert_ratio(ratio));
-    if (self.start - val).abs() < (self.end - val).abs() {
-      RangeSliderPart::Start
-    } else if (self.start - val).abs() > (self.end - val).abs() {
-      RangeSliderPart::End
-    } else if ratio < self.start_ratio() {
-      RangeSliderPart::Start
-    } else {
-      RangeSliderPart::End
-    }
-  }
-
-  fn set_start_ratio(&mut self, ratio: f32) { self.set_start(self.convert_ratio(ratio)); }
-
-  fn set_end_ratio(&mut self, ratio: f32) { self.set_end(self.convert_ratio(ratio)); }
-
-  fn start_ratio(&self) -> f32 { self.calc_ratio(self.start) }
-
-  fn end_ratio(&self) -> f32 { self.calc_ratio(self.end) }
 
   pub fn set_start(&mut self, start: f32) {
     self.start = self.snap_v(start.clamp(self.min, self.end.max(self.min)));
@@ -345,27 +430,45 @@ impl RangeSlider {
     self.set_end(self.end);
   }
 
-  fn stop_indicator_track(&self) -> Option<BoxFnWidget<'static>> {
-    let divisions = self.divisions?;
-    let start = (self.start_ratio() * divisions as f32).round() as usize;
-    let end = (self.end_ratio() * divisions as f32).round() as usize;
-    Some(stop_indicator_track(divisions + 1, start..=end, vec![start, end]))
+  fn choose_part(&self, ratio: f32) -> RangeSliderPart {
+    let val = self.snap_v(self.convert_ratio(ratio));
+    if (self.start - val).abs() < (self.end - val).abs() {
+      RangeSliderPart::Start
+    } else if (self.start - val).abs() > (self.end - val).abs() {
+      RangeSliderPart::End
+    } else if ratio < self.start_ratio() {
+      RangeSliderPart::Start
+    } else {
+      RangeSliderPart::End
+    }
   }
+
+  fn set_start_ratio(&mut self, ratio: f32) { self.set_start(self.convert_ratio(ratio)); }
+
+  fn set_end_ratio(&mut self, ratio: f32) { self.set_end(self.convert_ratio(ratio)); }
+
+  fn start_ratio(&self) -> f32 { self.calc_ratio(self.start) }
+
+  fn end_ratio(&self) -> f32 { self.calc_ratio(self.end) }
+}
+
+impl SliderCore for RangeSlider {
+  fn min(&self) -> f32 { self.min }
+  fn max(&self) -> f32 { self.max }
+  fn divisions(&self) -> Option<usize> { self.divisions }
 }
 
 impl Compose for RangeSlider {
   fn compose(this: impl StateWriter<Value = Self>) -> Widget<'static> {
     fn_widget! {
-      let mut row = @Flex { align_items: Align::Center };
+      let mut track = @Flex { align_items: Align::Center };
+      let track_width = track.layout_width();
       let active_part = Stateful::new(RangeSliderPart::Start);
 
       @PointerSelectRegion {
         on_custom: move |e: &mut PointerSelectEvent| {
-          let width = *$read(row.layout_width());
-          if width <= 0. {
-            return;
-          }
-
+          let width = *$read(track_width);
+          if width <= 0. { return; }
           let (_, to) = e.data().endpoints();
           let mut active_part = $write(active_part);
           if let PointerSelectData::Start(p) = e.data() {
@@ -376,18 +479,11 @@ impl Compose for RangeSlider {
         },
         @Stack {
           class: SLIDER_CONTAINER,
-          @(row) {
+          @(track) {
             v_align: VAlign::Center,
             @Expanded {
               flex: pipe!($read(this).start_ratio()),
               @Void { class: RANGE_SLIDER_INACTIVE_TRACK_LEFT }
-            }
-            @Void {
-              class: SLIDER_INDICATOR,
-              tooltips: pipe! {
-                let this = $read(this);
-                slider_tooltip(this.min, this.max, this.start)
-              },
             }
             @Expanded {
               flex: pipe! {
@@ -396,59 +492,19 @@ impl Compose for RangeSlider {
               },
               @Void { class: RANGE_SLIDER_ACTIVE_TRACK }
             }
-            @Void {
-              class: SLIDER_INDICATOR,
-              tooltips: pipe! {
-                let this = $read(this);
-                slider_tooltip(this.min, this.max, this.end)
-              },
-            }
             @Expanded {
               flex: pipe!(1. - $read(this).end_ratio()),
               @Void { class: RANGE_SLIDER_INACTIVE_TRACK_RIGHT }
             }
           }
-          @{ pipe!($read(this).stop_indicator_track()) }
+          @InParentLayout {
+            @RangeSlider::thumb_container($writer(this))
+          }
         }
       }
     }
     .into_widget()
   }
-}
-
-fn stop_indicator_track(
-  cnt: usize, actives: std::ops::RangeInclusive<usize>, filter: Vec<usize>,
-) -> BoxFnWidget<'static> {
-  fn_widget!(
-    let stop_builder = move |i| {
-      @Void {
-        class: if actives.contains(&i) {
-          STOP_INDICATOR_ACTIVE
-        } else {
-          STOP_INDICATOR_INACTIVE
-        },
-        visible: !filter.contains(&i),
-      }
-    };
-
-    // ReWrap FatObj to get the whole stop Widget's layout
-    let mut last = FatObj::new(stop_builder(cnt - 1));
-    let mut flex =  @Flex {
-        align_items: Align::Center,
-        justify_content: JustifyContent::SpaceBetween
-    };
-
-    @IgnorePointer {
-      @(flex) {
-        v_align: VAlign::Center,
-        opacity: pipe!($read(last.layout_rect()).max_x() <= *$read(flex.layout_width()) + 0.001)
-          .map(|v| if v { 1. } else { 0. }),
-        @ {(0..cnt-1).map(stop_builder)}
-        @{ last }
-      }
-    }
-  )
-  .boxed()
 }
 
 #[cfg(test)]
@@ -468,6 +524,24 @@ mod tests {
       @Slider { value: 32., divisions: Some(10) }
       @RangeSlider { start: 10., end: 73. }
       @RangeSlider { start: 10., end: 73., divisions: Some(10) }
+    })
+    .with_wnd_size(Size::new(300., 200.))
+    .with_comparison(0.0002)
+  );
+
+  widget_image_tests!(
+    slider_ticks_widgets,
+    WidgetTester::new(providers! {
+      providers: [Provider::new(SliderTicks::Always)],
+      @flex! {
+        direction: Direction::Vertical,
+        justify_content: JustifyContent::SpaceAround,
+        align_items: Align::Center,
+        @Slider { value: 32. }
+        @Slider { value: 32., divisions: Some(10) }
+        @RangeSlider { start: 10., end: 73. }
+        @RangeSlider { start: 10., end: 73., divisions: Some(10) }
+      }
     })
     .with_wnd_size(Size::new(300., 200.))
     .with_comparison(0.0002)
@@ -639,5 +713,53 @@ mod tests {
     range.set_end_ratio(ratio);
     assert!((range.start() - 50.).abs() < 1e-5);
     assert!((range.end() - 60.).abs() < 1e-5);
+  }
+  #[test]
+  fn slider_click_update() {
+    reset_test_env!();
+    let (value, w_value) = split_value(0.);
+    let w = fn_widget! {
+      let slider = @Slider { value: 0., max: 100. };
+      watch!($read(slider).value())
+        .subscribe(move |v| *$write(w_value) = v);
+      @SizedBox {
+        size: Size::new(100., 20.),
+        @ { slider }
+      }
+    };
+
+    let wnd = TestWindow::new_with_size(w, Size::new(100., 20.));
+    wnd.draw_frame();
+
+    wnd.process_cursor_move(Point::new(50., 10.));
+    wnd.process_mouse_press(Box::new(DummyDeviceId), MouseButtons::PRIMARY);
+    wnd.draw_frame();
+    assert!((*value.read() - 50.).abs() < 1e-5);
+  }
+
+  #[test]
+  fn range_slider_click_update() {
+    reset_test_env!();
+    let (value, w_value) = split_value(RangeSliderValue { start: 0., end: 100. });
+    let w = fn_widget! {
+      let slider = @RangeSlider { start: 10., end: 90., max: 100. };
+      watch!($read(slider).value())
+        .subscribe(move |v| *$write(w_value) = v);
+      @SizedBox {
+        size: Size::new(100., 20.),
+        @ { slider }
+      }
+    };
+
+    let wnd = TestWindow::new_with_size(w, Size::new(100., 20.));
+    wnd.draw_frame();
+
+    // Click at 20% (nearer to start)
+    wnd.process_cursor_move(Point::new(20., 10.));
+    wnd.process_mouse_press(Box::new(DummyDeviceId), MouseButtons::PRIMARY);
+    wnd.draw_frame();
+    let value = value.read();
+    assert!((value.start - 20.).abs() < 1e-5);
+    assert!((value.end - 90.).abs() < 1e-5);
   }
 }
