78fab3a6fdcb6aeac8177dfa449ae5135a2ec07e
Adoo <Adoo@outlook.com>
2026-01-12 14:46:31 +0800

    fix(widgets): 🐛 support click-to-update for slider and refactor layout
    - Update `PointerSelectRegion` to trigger selection start on pointer down.
    - Refactor `Slider` and `RangeSlider` to use `InParentLayout` for indicators, separating them from the track layout.
    - Update material theme definitions and update visual tests.

--- widgets/src/select_region.rs
+++ widgets/src/select_region.rs
@@ -38,30 +38,23 @@ impl<'c> ComposeChild<'c> for PointerSelectRegion {
       let from = Stateful::new(None);
       @(child) {
         on_pointer_down: move |e| {
-          *$write(from) = Some(e.position());
+          if e.mouse_buttons() == MouseButtons::PRIMARY {
+            let pos = e.position();
+            *$write(from) = Some(pos);
+            *$write(grab_handle) = Some(GrabPointer::grab(e.current_target(), &e.window()));
+            notify_select_changed(e.current_target(), PointerSelectData::Start(pos), &e.window());
+          }
         },
         on_pointer_move: move |e| {
-          let mut grab_handle = $write(grab_handle);
-          let mut from = $write(from);
-          if grab_handle.is_none()
-            && from.is_some()
-            && e.mouse_buttons() == MouseButtons::PRIMARY {
-            *grab_handle = Some(GrabPointer::grab(e.current_target(), &e.window()));
+          let from = *$read(from);
+          if let Some(from) = from
+            && $read(grab_handle).is_some()
+          {
             notify_select_changed(
               e.current_target(),
-              PointerSelectData::Start((*from).unwrap()),
+              PointerSelectData::Move { from, to: e.position() },
               &e.window()
             );
-          }
-          if grab_handle.is_some() {
-            let from = from.unwrap();
-            notify_select_changed(
-              e.current_target(),
-              PointerSelectData::Move{ from, to: e.position() },
-              &e.window()
-            );
-          } else {
-            from.take();
           }
         },
         on_pointer_up: move |e| {
@@ -71,7 +64,7 @@ impl<'c> ComposeChild<'c> for PointerSelectRegion {
           {
             notify_select_changed(
               e.current_target(),
-              PointerSelectData::End{ from, to: e.position() },
+              PointerSelectData::End { from, to: e.position() },
               &e.window()
             );
           }
