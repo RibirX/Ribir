f3c50d67226100651ab59250608edcb001c780ca
Adoo <Adoo@outlook.com>
2026-03-24 15:55:07 +0800

    refactor(algo): 💡 re-export Arc from rclite and update style guide

--- algo/src/lib.rs
+++ algo/src/lib.rs
@@ -5,6 +5,6 @@ mod frame_cache;
 pub use cow_rc::{CowArc, Substr};
 pub use frame_cache::*;
 mod resource;
-// Re-export rclite::Rc for convenience
-pub use rclite::Rc;
+// Re-export rclite::Rc and Arc for convenience
+pub use rclite::{Arc, Rc};
 pub use resource::*;
